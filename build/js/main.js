17d0ad33e620595ed502492db1da45c4c407f8cf
mattgrosso <mattgrosso@gmail.com>
2016-11-08 00:08:09 -0500

    Built first version of creature maker.

---
+++ build/js/main.js
@@ -0,0 +1,77 @@
+(function() {
+  'use strict';
+
+  var creaturePool = [];
+
+  // Set up a random number of random creatures.
+  function seedCreatures(total, counter) {
+    console.log('This is a flag');
+    console.log('counter: ', counter);
+    console.log('total: ', total);
+    
+    counter = counter || 1;
+
+    var randomMom = {
+      gender: 'female',
+      size: Math.floor(Math.random()*10)+1,
+      speed: Math.floor(Math.random()*10)+1
+    };
+    var randomDad = {
+      gender: 'male',
+      size: Math.floor(Math.random()*10)+1,
+      speed: Math.floor(Math.random()*10)+1
+    };
+
+    creaturePool.push(createCreature(randomMom, randomDad));
+
+    if (counter === total) {
+      return 'done';
+    } else {
+      counter ++;
+      seedCreatures(total, counter);
+    }
+
+  }
+
+  seedCreatures(10);
+
+
+  // Function to create a creature.
+  function createCreature(mom, dad) {
+    var randomGender = Math.ceil(Math.random()*10000)+1;
+    var randomSpeed = Math.floor(Math.random()*11)-5;
+    var randomSize = Math.floor(Math.random()*11)-5;
+    var babyCreature = {};
+
+    if (randomGender % 2 === 0) {
+      babyCreature.gender = 'female';
+    } else {
+      babyCreature.gender = 'male';
+    }
+
+    if (((mom.speed + dad.speed)/2 + randomSpeed) > 0) {
+      babyCreature.speed = (mom.speed + dad.speed)/2 + randomSpeed;
+    } else {
+      babyCreature.stillBorn = true;
+      console.log(babyCreature);
+    }
+
+    if (((mom.size + dad.size)/2 + randomSize) > 0) {
+      babyCreature.size = (mom.size + dad.size)/2 + randomSize;
+    } else {
+      babyCreature.tooSmall = true;
+      console.log(babyCreature);
+    }
+
+    return babyCreature;
+  }
+
+  // Have them all move around and make decisions based on values I set.
+
+  // function loop() {
+  //
+  // }
+
+  // var looper = setInterval(loop, 20);
+
+})();
